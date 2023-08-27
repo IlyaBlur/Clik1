f064a23a61b97d5e75dc8ce128719ddb4bfcaf94
IlyaBlur <ilya.kormilitcin@gmail.com>
2023-08-27 15:04:21 +0300

    Auto clicker Hotkey ALT+X

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import keyboard
 import mouse
 import time
+from plyer import notification
+
+
+def show_notification(title, message):
+    notification.notify(title=title,
+                        message=message,
+                        app_icon="icon.ico",
+                        timeout=1)
+
 
 IsClicking = False
 
@@ -8,6 +17,10 @@ IsClicking = False
 def start_Clicking():
     global IsClicking
     IsClicking = not IsClicking
+    if IsClicking:
+        show_notification("Кликер", "Кликер включен")
+    else:
+        show_notification("Кликер", "Кликер выключен")
 
 
 keyboard.add_hotkey('Alt + X', start_Clicking)
